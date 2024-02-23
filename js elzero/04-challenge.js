b0b4f4b3dd4cb2ed9cb9c155b5a940c973ed9012
mohamed-siradj <siradjmakhloufiled@gmail.com>
2024-02-23 08:29:25 +0100

    update

---
+++ js elzero/04-challenge.js
@@ -0,0 +1,13 @@
+// if challenge 
+let a = 7;
+a < 10 ? console.log(10) :
+a >= 10 && a <= 40 ? console.log("10 To 40") :
+a >10 ? console.log("> 40") : console.log("into the unknown");
+
+// write with teranry if syntax 
+let st = "elzero web school";
+// first  
+typeof st === typeof "10" ? console.log("good") : console.log("lala moh");
+// second  
+st !== "number" ? console.log("good") : console.log("lala moh");
+// third
