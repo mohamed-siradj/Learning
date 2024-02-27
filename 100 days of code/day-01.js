8565bfd3aef13859f9679937d261e9c9d307744c
mohamed-siradj <siradjmakhloufiled@gmail.com>
2024-02-27 10:40:38 +0100

    update

--- 100 days of code/day-01.js
+++ 100 days of code/day-01.js
@@ -1,36 +1,26 @@
-/*
 function showTime() {
-  var date = new Date();
-  var h = date.getHours(); // 0 - 23
-  var m = date.getMinutes(); // 0 - 59
-  var s = date.getSeconds(); // 0 - 59
-  var session = "AM";
+  let date = new Date();
+  let hour = date.getHours(); // 0 - 23
+  let min = date.getMinutes(); // 0 - 59
+  let sec = date.getSeconds(); // 0 - 59
+  let session = "AM";
 
-  if (h == 0) {
-    h = 12;
-  }
-
-  if (h > 12) {
-    h = h - 12;
+  if (hour == 0) {
+    hour = 12;
+  } else if (hour > 12) {
+    hour -= 12;
     session = "PM";
   }
 
-  h = (h < 10) ? "0" + h : h;
-  m = (m < 10) ? "0" + m : m;
-  s = (s < 10) ? "0" + s : s;
+  hour = (hour < 10) ? "0" + hour : hour;
+  min = (min < 10) ? "0" + min : min;
+  sec = (sec < 10) ? "0" + sec : sec;
 
-  var time = h + ":" + m + ":" + s + " " + session;
+  let time = hour + ":" + min + ":" + sec + " " + session;
   document.getElementById("MyClockDisplay").innerText = time;
   document.getElementById("MyClockDisplay").textContent = time;
 
   setTimeout(showTime, 1000);
-
 }
 
-*/
-const date = new Date();
-
-const month = date.getMonth()
-const min = date.getMinutes()
-console.log(month);
-console.log(min);
+showTime();
