23a69109faf1d7e0b900d7036aa6d1be79abbf85
mohamed-siradj <siradjmakhloufiled@gmail.com>
2024-03-05 10:27:41 +0100

    update script.js

--- 100 days of code/day-04/script.js
+++ 100 days of code/day-04/script.js
@@ -4,46 +4,34 @@ const oClass = 'o-player';
 let resetBtn = document.querySelector('.reset');
 let cells = document.querySelectorAll('[box]');
 let arr = [];
-let checkWin = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
+
 // check winner 
 function winner() {
   
-  for (var i = 0; i < cells.length; i++) {
-    
-    arr[i] = `${cells[i].classList.value}moh`;
-    console.log(arr[i]);
-    
-    
-    
-    /* 
-    
+  
+}
+    /*
+  
     let option = checkWin[i];
-    // return the first prob win array
     let a = option[0];
     let b = option[1];
     let c = option[2];
-    
+  
     // compare the cells
-    if (cells[a].classList.value === cells[b].classList.value && cells[b].classList.value === cells[c].classList.value && cells[a].classList.value !== ''){
-      console.log(`${cells[a].classList.value} win`);
+    if (arr[a] === arr[b] && arr[b] === arr[c] && arr[a] !== '') {
+      console.log(`${arr[a]} win`);
     }
-    */
   }
-};
+  */
+
 
 // put mark Done T T
 for (var i = 0; i < cells.length; i++) {
   
-   cells[i].addEventListener('click', play, {once : true});
+   cells[i].addEventListener('click', play);
+   //.onclick = play;
+   //.addEventListener('click', play, {once : true});
+  arr[i] = cells[i].className;
 }
 function play() { 
   
@@ -57,15 +45,16 @@ function play() {
     playerTurn = 'x';
     
   }
-  
- // console.log('clicked');
 }
 
 // reset button
+// i don't know how it work 
+// but it work 
 resetBtn.addEventListener('click',resetGame);
 function resetGame() {
   
   for (var i = 0; i < cells.length; i++) {
     cells[i].classList.remove(xClass,oClass);
   }
+  playerTurn = 'x'
 }
