781d21e1d59c3c4c4fba287d221cbf1f5871d318
mohamed-siradj <siradjmakhloufiled@gmail.com>
2024-03-11 02:57:34 +0100

    update

---
+++ 100 days of code/day-05/script.js
@@ -0,0 +1,48 @@
+let btn = document.getElementById('btn');
+let inp = document.getElementById('inp');
+let boxes = document.querySelectorAll('.box');
+let drag = null;
+// first step done ^^
+btn.onclick = function () {
+  if (inp.value !== '') {
+    let prg = document.createElement('p');
+    prg.className = 'item';
+    let text = document.createTextNode(inp.value);
+    prg.appendChild(text);
+    boxes[0].appendChild(prg);
+    inp.value = '';
+  }
+  dragAndDrop();
+}
+function dragAndDrop() {
+  let items = document.querySelectorAll('.item'); 
+  items.forEach(item => {
+    item.addEventListener('dragstart',function () {
+      drag = item;
+      item.style.opacity = '0.5';
+    });
+    
+    item.addEventListener('dragend',function () {
+      drag = null;
+    });
+  });
+  
+    boxes.forEach(box => {
+      box.addEventListener('dragover',function (e) {
+        e.preventDefault();
+        this.style.background = '#9FF041';
+        this.style.color = '#fff';
+      });
+      
+      box.addEventListener('dragleave',function () {
+        this.style.background = '#fff';
+        this.style.color = '#000';
+      });
+      
+      box.addEventListener('drop',function () {
+        this.append(drag);
+        this.style.background = '#fff';
+        this.style.color = '#000';
+      });
+    });
+}
